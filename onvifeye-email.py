ad69f02c5ea93aa603970a0a42b2bc47d18b52b7
digitaltrails <michael@actrix.gen.nz>
2025-02-13 16:27:18 +1300

    HTML email with viewable jpg.

--- onvifeye-email.py
+++ onvifeye-email.py
@@ -4,9 +4,8 @@ import logging
 import smtplib
 import sys
 import time
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from email.message import EmailMessage
+from email.utils import make_msgid
 from email.utils import formatdate
 from pathlib import Path
 from typing import List
@@ -14,21 +13,35 @@ from typing import List
 log = logging.getLogger('onvifemail')
 
 def send_mail(send_from: str, send_to: List[str],
-              subject: str, message: str, files: List[Path],
-              server="localhost", port=587, username='', password=''):
-    msg = MIMEMultipart()
+              subject: str, message: str, jpeg_filename: Path,
+              server="localhost", port=587, username='', password='', add_legal_stuff=False):
+    msg = EmailMessage()
     msg['From'] = send_from
     msg['To'] = ", ".join(send_to)
     msg['Subject'] = subject
     msg['Date'] = formatdate(localtime=True)
-    msg.attach(MIMEText(message))
-    for file_path in files:
-        with open(file_path, "rb") as fd:
-            ext = file_path.suffix
-            attachment = MIMEApplication(fd.read(), _subtype=ext)
-            attachment.add_header(
-                'content-disposition', 'attachment', filename=file_path.name)
-        msg.attach(attachment)
+
+    attachment_cid = make_msgid()
+
+    boilerplate = """
+     Please notify the sender immediately by e-mail if you have 
+     received this e-mail by mistake and delete this e-mail from
+     your system. If you are not the intended recipient you are 
+     notified that disclosing, copying, distributing or taking 
+     any action in reliance on the contents of this information 
+     is strictly prohibited.""" if add_legal_stuff else ''
+
+    msg.set_content(f'{message}\n\n{boilerplate}')
+    html_message = message.replace("\n","<br/>")
+    msg.add_alternative(f'<html><body><br/><b>{html_message}</b><br/><br/>'
+                        f'<img src="cid:{attachment_cid}"/>'
+                        f'<br/><br/>{boilerplate}</body></html>',
+                        'html')
+
+    if jpeg_filename:
+        with open(jpeg_filename, "rb") as fd:
+            msg.get_payload()[1].add_related(fd.read(), 'image', 'jpeg', cid=attachment_cid)
+
     smtp_connection = smtplib.SMTP(host=server, port=port)
     smtp_connection.starttls()
     smtp_connection.login(username, password)
@@ -51,16 +64,16 @@ def main():
         message = (f'Camera: {camera_id}\n\n' +
                    '\n'.join([ f'{k.removeprefix("Is")} detected at {v}'
                               for k, v in detections.items()]))
-        files = []
-        attachment = Path.home() / 'onvifeye' / 'images' / camera_id / f'{list(detections.values())[0]}.jpg'
-        print(attachment.as_posix())
+        attach_filename = None
+        jpeg_filename = Path.home() / 'onvifeye' / 'images' / camera_id / f'{list(detections.values())[0]}.jpg'
+        print(jpeg_filename.as_posix())
         for _ in range(10):  # give up after 10 seconds
-            if attachment.exists():
-                files.append(attachment)
+            if jpeg_filename.exists():
+                attach_filename = jpeg_filename
                 break
             print("sleeping")
             time.sleep(1)
-        send_mail(**email_config, subject=subject, message=message, files=files)
+        send_mail(**email_config, subject=subject, message=message, jpeg_filename=attach_filename)
 
 if __name__ == '__main__':
     main()
